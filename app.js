c404c6b092fb298a5eef9d4e0bbc03f4ee6dd451
Juan Carlos Jimenez <jcjj.system@gmail.com>
2015-08-01 22:30:47 -0400

    api delete and list functions

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 
-app.get('/crud', crud.test);
+app.get('/crud/api/list', crud.list);
+app.get('/crud/api/add', crud.add);
+app.get('/crud/api/delete', crud.add);
 
 var server = app.listen(6000);
--- crud.js
+++ crud.js
@@ -1,3 +1,51 @@
-exports.test = function(req, res) {
-	res.send('CRUD example');
+var SALT_WORK_FACTOR = 10;
+var mongoskin = require('mongoskin');
+var db = mongoskin.db('mongodb://localhost:27017/ms');
+
+// LIST users function
+exports.list = function(req, res) {
+	db.bind('msusers');
+	var criteria = {};
+	var options = {
+		"limit": 100, // TODO: limit by a external param
+		"sort": [['_id',1]]
+	}
+	db.msusers.find(criteria, options)
+						.toArray(function(e, results){
+              if (e) return next(e);
+              	for (i in results) {
+              		delete results[i].password;
+              	}
+              	res.send(results);
+      			});
+}
+
+// ADD users function
+exports.add = function(req, res) {
+	db.bind('msusers');
+	var bcrypt = require('bcrypt');
+
+	// Auto-Incrementing Sequence UserID
+  db.collection('counters').findAndModify({ _id: "userid" }, {}, { $inc: { seq: 1 } }, { new: true }, function(err, doc) { 
+		var user = {
+	     "_id": doc.seq,
+	     "name": "Juan Carlos",
+	     "last_name": "Jimenez",
+	     "email": "jc@jcjimenez.me",
+	     "bdate": "31-05-1976",
+	     "password": "password"
+		}
+
+		bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+	    bcrypt.hash(user.password, salt, function(err, hash) {
+	      user.password = hash;
+	      db.msusers.insert(user,{}, function(){
+	        console.log(arguments);
+	      });
+	      user.added = "true";
+	      res.send(user);
+	    });
+		});
+
+	})
 }
